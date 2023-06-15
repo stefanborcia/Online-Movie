04d547891b714653dd12c392508ee9ee8ba2fc06
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-06-15 23:43:12 +0200

    Create Category

--- Asp.Net_MVC/Models/Category.cs
+++ Asp.Net_MVC/Models/Category.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Asp.Net_MVC.Models
 {
     public class Category
     {
-
+        [Key]
         public int CategoryId { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
