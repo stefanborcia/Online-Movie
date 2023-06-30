dd98c492fc1928a9d6813a63dea4c533ffeba332
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-06-30 23:23:48 +0200

    Scaffold Identity

--- Asp.Net_MVC/Program.cs
+++ Asp.Net_MVC/Program.cs
@@ -1,7 +1,8 @@
-using Movie_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using Movie_DataAccess.Data;
 using Movie_DataAccess.Repository;
 using Movie_DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -27,6 +31,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
--- Movie_DataAccess/Data/ApplicationDbContext.cs
+++ Movie_DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Movie_Models;
 
 namespace Movie_DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
