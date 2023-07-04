1739b3aa4c0b8d73c7130cb09c95917b2b822426
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-07-04 23:31:17 +0200

    implement Company Role, add migration CompanyTable, update database, create Company Controller, Create Company Model and Interface for Company Repository

---
+++ Asp.Net_MVC/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Movie_DataAccess.Data;
+using Movie_DataAccess.Repository.IRepository;
+using Movie_Models;
+using Movie_Models.ViewModels;
+using Movie_Utility;
+
+namespace Asp.Net_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //[Route("Categories")
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+
+            return View(objCompanyList);
+        }
+
+        public IActionResult UpSert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                // Create
+                return View(new Company());
+            }
+            else
+            {
+                // Update
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+        }
+        [HttpPost]
+        public IActionResult UpSert(Company CompanyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return View(CompanyObj);
+            }
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objCompanyList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var CompanyToBeDelete = _unitOfWork.Company.Get(u => u.Id == id);
+            if (CompanyToBeDelete == null)
+            {
+                return Json(new { success = false, message = "Error while deleting..." });
+            }
+
+            _unitOfWork.Company.Remove(CompanyToBeDelete);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
