e0725710e7320f8f8da690a7f13d15404e947daf
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-07-12 22:56:53 +0200

    confirm Stripe Payment and order placed Successfully with Stripe

--- Asp.Net_MVC/Areas/Customer/Controllers/CartController.cs
+++ Asp.Net_MVC/Areas/Customer/Controllers/CartController.cs
@@ -167,6 +167,25 @@ namespace Asp.Net_MVC.Areas.Customer.Controllers
 
         public IActionResult OrderConfirmation(int id)
         {
+	        OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "ApplicationUser");
+	        if (orderHeader.PaymentStatus != StaticDetails.PaymentStatusDelayedPayment)
+	        {
+		        var service = new SessionService();
+		        Session session = service.Get(orderHeader.SessionId);
+		        if (session.PaymentStatus.ToLower() == "paid")
+		        {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id,session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(id,StaticDetails.StatusApproved,StaticDetails.PaymentStatusApproved);
+                    _unitOfWork.Save();
+		        }
+	        }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+
+
             return View(id);
         }
 
