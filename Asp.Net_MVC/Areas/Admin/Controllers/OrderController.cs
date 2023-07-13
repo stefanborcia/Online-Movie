69ada44ebdd75eedd63cd8b17de2acd8bdaa982d
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-07-13 21:38:19 +0200

    create orderviewmodel, order controller and order list UI

---
+++ Asp.Net_MVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Movie_DataAccess.Repository;
+using Movie_DataAccess.Repository.IRepository;
+using Movie_Models;
+
+namespace Asp.Net_MVC.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}
---
+++ Asp.Net_MVC/wwwroot/js/order.js
@@ -0,0 +1,29 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $("#myTable").DataTable({
+        "ajax": { url:"/admin/order/getall" },
+        "columns":
+            [
+                { data: "id", "width": "20%" },
+                { data: "name", "width": "10%" },
+                { data: "phoneNumber", "width": "20%" },
+                { data: 'applicationUser.email', "width": "15%" },
+                { data: "orderStatus", "width": "15%" },
+                { data: "orderTotal", "width": "15%" },
+                {
+                    data: "productId",
+                    "render": function (data) {
+                        return `<div class="w-100 btn-group" role="group">
+                                   <a href="/Admin/Product/UpSert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                                </div>`;
+                    },
+                    "width": "25%"
+                }
+            ]
+    });
+}
