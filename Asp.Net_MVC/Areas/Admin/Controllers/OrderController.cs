6c49bab29fc01374b9e36fdf9f8d779210ca4480
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-07-14 23:40:56 +0200

    update index order view

--- Asp.Net_MVC/Areas/Admin/Controllers/OrderController.cs
+++ Asp.Net_MVC/Areas/Admin/Controllers/OrderController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Movie_DataAccess.Repository;
 using Movie_DataAccess.Repository.IRepository;
 using Movie_Models;
+using System.Diagnostics;
+using Movie_Utility;
 
 namespace Asp.Net_MVC.Areas.Admin.Controllers
 {
@@ -26,10 +28,32 @@ namespace Asp.Net_MVC.Areas.Admin.Controllers
 		#region API CALLS
 
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeaders });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+
+					break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion
--- Asp.Net_MVC/wwwroot/js/order.js
+++ Asp.Net_MVC/wwwroot/js/order.js
@@ -1,25 +1,42 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.include("inprocess")) {
+        loadDataTable("inprocess");
+    } else {
+        if (url.include("completed")) {
+            loadDataTable("completed");
+        } else {
+            if (url.include("pending")) {
+                loadDataTable("pending");
+            } else {
+                if (url.include("approved")) {
+                    loadDataTable("approved");
+                } else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
 });
 
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $("#myTable").DataTable({
-        "ajax": { url:"/admin/order/getall" },
+        "ajax": { url:"/admin/order/getall?status=" + status },
         "columns":
             [
-                { data: "id", "width": "20%" },
-                { data: "name", "width": "10%" },
+                { data: "id", "width": "5%" },
+                { data: "name", "width": "15%" },
                 { data: "phoneNumber", "width": "20%" },
-                { data: 'applicationUser.email', "width": "15%" },
+                { data: 'applicationUser.email', "width": "20%" },
                 { data: "orderStatus", "width": "15%" },
                 { data: "orderTotal", "width": "15%" },
                 {
                     data: "productId",
                     "render": function (data) {
                         return `<div class="w-100 btn-group" role="group">
-                                   <a href="/Admin/Product/UpSert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                                   <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
                                 </div>`;
                     },
                     "width": "25%"
