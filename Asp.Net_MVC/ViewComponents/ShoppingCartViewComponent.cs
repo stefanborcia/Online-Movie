e8d074c84005b4f7c73f79fa28b3804bebd7922a
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-07-16 15:47:00 +0200

    update _Layout, and create DefaultView

--- Asp.Net_MVC/ViewComponents/ShoppingCartViewComponent.cs
+++ Asp.Net_MVC/ViewComponents/ShoppingCartViewComponent.cs
@@ -1,6 +1,39 @@
-﻿namespace Asp.Net_MVC.ViewComponents
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie_DataAccess.Repository.IRepository;
+using Movie_Utility;
+using System.Security.Claims;
+
+namespace Asp.Net_MVC.ViewComponents
 {
-    public class ShoppingCartViewComponent
+    public class ShoppingCartViewComponent: ViewComponent
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity!;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(StaticDetails.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(StaticDetails.SessionCart, _unitOfWork.ShoppingCart.GetAll(u =>
+                        u.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(StaticDetails.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
     }
 }
